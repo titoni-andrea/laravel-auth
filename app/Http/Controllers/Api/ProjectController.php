03036a23c828b36cbf1983a4b5bb302250d5e854
titoni-andrea <andreatitoni@gmail.com>
2024-07-18 15:26:29 +0200

    controller api

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        return response()->json([
+            'success' => true,
+            'project' => Project::orderByDesc('id')->paginate
+            ()
+        ]);
+        
+    }
+}
